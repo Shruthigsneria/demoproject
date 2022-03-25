245d1bb38bce5859af70a93a080a121792ea6f97
shruthi <shruthigsneria@gmail.com>
2022-03-25 14:01:03 +0530

    changes

---
+++ realproject/src/test/java/organisationpacakage/CreateProductTest.java
@@ -0,0 +1,19 @@
+package organisationpacakage;
+
+import org.testng.annotations.Test;
+
+public class CreateProductTest {
+	@Test
+	public void createproduct() {
+		
+		
+		System.out.println("product created");
+		
+		
+		
+		
+		
+	}
+	
+
+}
