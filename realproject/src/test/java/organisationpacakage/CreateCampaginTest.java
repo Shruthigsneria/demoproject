28ce826acfa0397f4b9eec960de9b901dbe0770d
shruthi <shruthigsneria@gmail.com>
2022-03-25 11:09:01 +0530

    new project 1st commit

---
+++ realproject/src/test/java/organisationpacakage/CreateCampaginTest.java
@@ -0,0 +1,13 @@
+package organisationpacakage;
+
+import org.testng.annotations.Test;
+
+public class CreateCampaginTest {
+	@Test
+	
+	public void createcamp() {
+		System.out.println("camp created");
+		
+	}
+
+}
---
+++ realproject/src/test/java/organisationpacakage/CreateOrgTest.java
@@ -0,0 +1,13 @@
+package organisationpacakage;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	@Test
+  
+	public void createorg() {
+		System.out.println("created org");
+		
+	}
+
+}
